a18281a22c8acbd8bb4762ac97dc399f7c9a6faa
LVILCARROMEROO <luis.evo_8.10@hotmail.com>
2020-06-24 23:09:28 -0500

    second

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,9 +1,48 @@
 import { Config } from '@stencil/core';
+import { angularOutputTarget, ValueAccessorConfig } from '@stencil/angular-output-target';
+
+const angularValueAccessorBindings: ValueAccessorConfig[] = [
+  {
+    elementSelectors: ['my-input[type=text]'],
+    event: 'myChange',
+    targetAttr: 'value',
+    type: 'text',
+  },
+  {
+    elementSelectors: ['my-input[type=number]'],
+    event: 'myChange',
+    targetAttr: 'value',
+    type: 'number',
+  },
+  {
+    elementSelectors: ['my-checkbox'],
+    event: 'myChange',
+    targetAttr: 'checked',
+    type: 'boolean',
+  },
+  {
+    elementSelectors: ['my-radio'],
+    event: 'mySelect',
+    targetAttr: 'checked',
+    type: 'radio',
+  },
+  {
+    elementSelectors: ['my-range', 'my-radio-group'],
+    event: 'myChange',
+    targetAttr: 'value',
+    type: 'select',
+  },
+];
 
 export const config: Config = {
   namespace: 'my-ds',
   taskQueue: 'async',
   outputTargets: [
+    angularOutputTarget({
+      componentCorePackage: 'component-library-angular',
+      directivesProxyFile: 'component-library-angular/src/directives/proxies.ts',
+      valueAccessorConfigs: angularValueAccessorBindings,
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader'
